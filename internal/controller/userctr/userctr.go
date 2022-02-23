65aaf75af055ba057962dbe345bdbdd88adb301b
Syukri Suwandha <syukri.suwandha@gmail.com>
2022-02-23 08:53:00 +0700

    add http userctr

--- internal/controller/userctr/userctr.go
+++ internal/controller/userctr/userctr.go
@@ -1,1 +1,31 @@
 package userctr
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func UserCtr(w http.ResponseWriter, r *http.Request) {
+	keys, ok := r.URL.Query()["key"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'key' is missing")
+		return
+	}
+
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+	key := keys[0]
+
+	log.Println("Url Param 'key' is: " + string(key))
+	fmt.Fprintf(w, "key = %s\n", key)
+}
+
+func UserCtrById(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/user/")
+
+	log.Println("Url Param 'id' is: " + string(id))
+	fmt.Fprintf(w, "ID = %s\n", id)
+}
